dbe3fde774fc215e3a456b7a0e059371a5789095
Rohit Kumar <rohit756567@gmail.com>
2025-09-08 11:14:18 +0530

    Making Search bar and some frontend

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,20 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      
+    <div className='grid grid-cols-5 h-screen text-center'>
+      <div className='col-span-1 bg-zinc-800'>
+
+      </div>
+      <div className='col-span-4 p-10'>
+        <div className='container h-110'>
+
+        </div>
+        <div className='bg-zinc-800 w-1/2 p-1 pr-5 text-white m-auto rounded-4xl
+        border border-zinc-700 flex h-16'>
+          <input type="text" className='w-full h-full p-3 outline-none' placeholder="Ask me anything" />
+          <button>Ask</button>
+        </div>
+      </div>
     </div>
   )
 }
