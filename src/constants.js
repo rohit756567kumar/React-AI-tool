040cd8fcea60261cbf993644bf9d5921b16258de
Rohit Kumar <rohit756567@gmail.com>
2025-09-09 11:09:27 +0530

    Update constants.js due to security breach

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const URL =
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyB4QIwn9ZHPkoSgP7te9zrEBMNgWuRFE8Q";
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=*****************************";
