1f4a6a9f2f7695e7bebc0bbcd670dc80dae6b3e5
Rohit Kumar <rohit756567@gmail.com>
2025-09-15 13:06:27 +0530

    theme change and optimize code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 import { URL } from "./constants";
 import Answer from "./components/Answers";
@@ -6,30 +6,48 @@ import Answer from "./components/Answers";
 function App() {
   const [question, setQuestion] = useState("");
   const [result, setResult] = useState([]);
-  const [recentHistory,setRecentHistory]=useState([])
+  const [recentHistory, setRecentHistory] = useState([]);
+  const [selectedHistory,setSelectedHistory]=useState('');
+  const scrollToAnswer = useRef();
+  const [loader,setLoader]=useState(false);
 
-  const payload = {
-    contents: [
-      {
-        parts: [
-          {
-            text: question,
-          },
-        ],
-      },
-    ],
-  };
+  useEffect(() => {
+    const savedHistory = JSON.parse(localStorage.getItem("history")) || [];
+    setRecentHistory(savedHistory);
+  }, []);
+
 
   const askQuestion = async () => {
-    if(localStorage.getItem('history')){
-      let history = JSON.parse(localStorage.getItem('history'))
-      history = [question,...history]
-      localStorage.setItem('history',JSON.stringify(history))
-      setRecentHistory(history)
-  }else{
-    localStorage.setItem("history", JSON.stringify([question]));
-    setRecentHistory([question])
-  }
+    if(!question){
+      return false;
+    }
+
+    if(question){
+          if (localStorage.getItem("history")) {
+            let history = JSON.parse(localStorage.getItem("history"));
+            history = [question, ...history];
+            localStorage.setItem("history", JSON.stringify(history));
+            setRecentHistory(history);
+          } else {
+            localStorage.setItem("history", JSON.stringify([question]));
+            setRecentHistory([question]);
+          }
+    }
+    const payloadData=question ? question:selectedHistory
+      const payload = {
+        contents: [
+          {
+            parts: [
+              {
+                text: payloadData,
+              },
+            ],
+          },
+        ],
+      };
+
+      setLoader(true);
+
     let response = await fetch(URL, {
       method: "post",
       body: JSON.stringify(payload),
@@ -41,17 +59,40 @@ function App() {
     dataString = dataString.map((item) => item.trim());
 
     //console.log(dataString);
-    setResult([...result,{type:'q',text:question},{type:'a' , text:dataString}]);
+    setResult([
+      ...result,
+      { type: "q", text: question?question:selectedHistory },
+      { type: "a", text: dataString },
+    ]);
+    setQuestion('')
+
+    setTimeout(()=>{
+      scrollToAnswer.current.scrollTop = scrollToAnswer.current.scrollHeight;
+    },500);
+    setLoader(false);
   };
 
   console.log(recentHistory);
-  const clearHistory = ()=>{
+  const clearHistory = () => {
     localStorage.clear();
-    setRecentHistory([])
+    setRecentHistory([]);
+  };
+
+  const isEnter=(event)=>{
+    console.log(event.key);
+    if(event.key=='Enter'){
+      askQuestion();
+    }
   }
+
+  useEffect(()=>{
+    askQuestion();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[selectedHistory])
+//in 2nd div we change recent search background color
   return (
     <div className="grid grid-cols-5 h-screen text-center">
-      <div className="col-span-1 bg-zinc-800 pt-3">
+      <div className="col-span-1 bg-[#201938] pt-3">
         <h1 className="text-xl text-white flex text-center justify-center">
           <span>Recent Search</span>
           <button onClick={clearHistory} className="cursor-pointer">
@@ -69,14 +110,45 @@ function App() {
         <ul className="text-left overflow-auto mt-2">
           {recentHistory &&
             recentHistory.map((item) => (
-              <li className="pl-5 px-5 truncate text-zinc-400 cursor-pointer hover:bg-zinc-700 hover:text-zinc-200">
+              <li
+                onClick={() => setSelectedHistory(item)}
+                className="pl-5 px-5 truncate text-zinc-400 cursor-pointer hover:bg-zinc-700 hover:text-zinc-200"
+              >
                 {item}
               </li>
             ))}
         </ul>
       </div>
       <div className="col-span-4 p-10">
-        <div className="container h-110 overflow-y-auto scrollbar-hide">
+        <h1 className="text-4xl bg-clip-text text-transparent bg-gradient-to-r from-[#556270] to-[#FF6B6B]">
+          Hey, Ask me Anything
+        </h1>
+        {loader ? (
+          <div role="status">
+            <svg
+              aria-hidden="true"
+              class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span class="sr-only">Loading...</span>
+          </div>
+        ) : null}
+
+        <div
+          ref={scrollToAnswer}
+          className="container h-110 overflow-y-auto scrollbar-hide"
+        >
           <div className="text-zinc-300">
             <ul>
               {result.map((item, index) => (
@@ -87,7 +159,7 @@ function App() {
                   {item.type === "q" ? (
                     <li
                       key={index + Math.random()}
-                      className="text-right p-1 border-8 bg-zinc-700 border-zinc-700 rounded-tl-3xl rounded-br-3xl rounded-bl-3xl w-fit"
+                      className="text-right p-1 border-8 bg-[#201938] border-[#201938] rounded-tl-3xl rounded-br-3xl rounded-bl-3xl w-fit"
                     >
                       <Answer
                         ans={item.text}
@@ -118,11 +190,12 @@ function App() {
         </div>
         <div
           className="bg-zinc-800 w-1/2 p-1 pr-5 text-white m-auto rounded-4xl
-        border border-zinc-700 flex h-16"
+        border border-[#287b8a] flex h-16"
         >
           <input
             type="text"
             value={question}
+            onKeyDown={isEnter}
             onChange={(event) => setQuestion(event.target.value)}
             className="w-full h-full p-3 outline-none"
             placeholder="Ask me anything"
